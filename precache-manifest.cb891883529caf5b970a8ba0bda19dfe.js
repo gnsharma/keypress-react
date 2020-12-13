65ff42fab81262bc7737872edc13a037d25f37b1
Govind N Sharma <gnsharma0810@gmail.com>
2020-12-13 13:46:32 +0530

    Updates

---
+++ precache-manifest.cb891883529caf5b970a8ba0bda19dfe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a10426294a1990e03b0f2407b353bfd",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9cd64fb31b774396db02",
+    "url": "./static/css/main.58a6113f.chunk.css"
+  },
+  {
+    "revision": "e9e800f6ab7062dfe730",
+    "url": "./static/js/2.c4cb3608.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.c4cb3608.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cd64fb31b774396db02",
+    "url": "./static/js/main.179b3135.chunk.js"
+  },
+  {
+    "revision": "803cca6169d2194a1073",
+    "url": "./static/js/runtime-main.0b1c6f58.js"
+  }
+]);
